6c785505939028c496348d1cce9274492845ae2d
marcos-x86 <marcos.x86@outlook.com>
2020-09-02 18:31:55 -0400

    Find intersection solution

---
+++ Katas/Marcos.Lara/Intersector.cs
@@ -0,0 +1,22 @@
+﻿namespace Katas.Marcos.Lara
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Defines Intersector utility class.
+    /// </summary>
+    public class Intersector
+    {
+        /// <summary>
+        /// Gets instersection.
+        /// </summary>
+        /// <param name="input">Input sequence.</param>
+        /// <returns>Intersection string.</returns>
+        public static string GetIntersection(string[] input)
+        {
+            string[] sequenceOne = input[0].Split(", ");
+            string[] sequenceTwo = input[1].Split(", ");
+            return string.Join(",", sequenceOne.Intersect(sequenceTwo));
+        }
+    }
+}
---
+++ KatasTests/Marcos.Lara/IntersectorTest.cs
@@ -0,0 +1,34 @@
+﻿namespace KatasTests.Marcos.Lara
+{
+    using Katas.Marcos.Lara;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines intersector test.
+    /// </summary>
+    [TestFixture]
+    public class IntersectorTest
+    {
+        /// <summary>
+        /// Tests intersector with basic input.
+        /// </summary>
+        [Test]
+        public void TestIntersector01()
+        {
+            string[] input = new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" };
+            string output = Intersector.GetIntersection(input);
+            Assert.AreEqual("1,4,13", output);
+        }
+
+        /// <summary>
+        /// Tests intersector with basic input.
+        /// </summary>
+        [Test]
+        public void TestIntersector02()
+        {
+            string[] input = new string[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" };
+            string output = Intersector.GetIntersection(input);
+            Assert.AreEqual("1,9,10", output);
+        }
+    }
+}
