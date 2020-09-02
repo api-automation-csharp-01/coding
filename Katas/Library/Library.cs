6897be4884387e7e6a2970e638571ce2216e4456
marcos-x86 <marcos.x86@outlook.com>
2020-09-01 23:51:23 -0400

    Base solution

---
+++ Katas/Library/Library.cs
@@ -0,0 +1,17 @@
+﻿namespace Katas.Library
+{
+    /// <summary>
+    /// Defines Library utility class.
+    /// </summary>
+    public class Library
+    {
+        /// <summary>
+        /// Checks validate flag.
+        /// </summary>
+        /// <returns>True in any case.</returns>
+        public static bool Validate()
+        {
+            return true;
+        }
+    }
+}
---
+++ KatasTests/LibraryTests/LibraryTest.cs
@@ -0,0 +1,21 @@
+﻿namespace KatasTests.LibraryTest
+{
+    using Katas.Library;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines Library utiliy class unit tests.
+    /// </summary>
+    [TestFixture]
+    public class LibraryTest
+    {
+        /// <summary>
+        /// Tests library flag.
+        /// </summary>
+        [Test]
+        public void TestLibraryValidation()
+        {
+            Assert.IsTrue(Library.Validate());
+        }
+    }
+}
