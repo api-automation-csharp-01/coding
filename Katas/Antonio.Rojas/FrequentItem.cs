97a7d4d4692a7c3a47c5427d6580b6d92c1da745
Antonio Rojas <arojas@pros.com>
2020-09-03 19:47:05 -0400

    Task 01 - TDD Programming Excercise

---
+++ Katas/Antonio.Rojas/FrequentItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Katas.Antonio.Rojas
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines FrequentItem utility class.
+    /// </summary>
+    public class FrequentItem
+    {
+        /// <summary>
+        /// Gets most frequest number.
+        /// </summary>
+        /// <param name="input">Input array.</param>
+        /// <returns>Most frequest number count.</returns>
+        public static int MostFrequentItemCount(int[] input)
+        {
+            var groups = input.GroupBy(x => x);
+            var largest = groups.OrderByDescending(x => x.Count()).First();
+            return largest.Count();
+        }
+    }
+}
