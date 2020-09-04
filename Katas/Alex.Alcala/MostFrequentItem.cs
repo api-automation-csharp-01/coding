0053f09a773956ddacb6e68fec1a407195b11e5f
alex alcala <Alex.Alcala@jalasoft.com>
2020-09-04 02:19:25 -0400

    Most Frequent Item Solution

---
+++ Katas/Alex.Alcala/MostFrequentItem.cs
@@ -0,0 +1,39 @@
+﻿namespace Katas.Alex.Alcala
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines MostFrequentItem utility class.
+    /// </summary>
+    public class MostFrequentItem
+    {
+        /// <summary>
+        /// MostFrequentItemCount.
+        /// </summary>
+        /// <param name="collection">Collect sequence.</param>
+        /// <returns>Most frequent Item.</returns>
+        public static int MostFrequentItemCount(int[] collection)
+        {
+            if (collection == null || collection.Length == 0)
+            {
+                return 0;
+            }
+
+            var newDict = new Dictionary<int, int>();
+            foreach (var number in collection)
+            {
+                if (newDict.ContainsKey(number))
+                {
+                    newDict[number] += 1;
+                }
+                else
+                {
+                   newDict.Add(number, 1);
+                }
+            }
+
+            return newDict.Values.Max();
+        }
+    }
+}
---
+++ KatasTests/Alex.Alcala/MostFrequentItemTest.cs
@@ -0,0 +1,34 @@
+﻿namespace KatasTests.Alex.Alcala
+{
+    using Katas.Alex.Alcala;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines MostFrequentItem test.
+    /// </summary>
+    [TestFixture]
+    public class MostFrequentItemTest
+    {
+        /// <summary>
+        /// Tests MostFrequentItem with basic input.
+        /// </summary>
+        [Test]
+        public void TestMostFrequentItem01()
+        {
+            int[] input = new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 };
+            int output = MostFrequentItem.MostFrequentItemCount(input);
+            Assert.AreEqual(5, output);
+        }
+
+        /// <summary>
+        /// Tests MostFrequentItem with basic input.
+        /// </summary>
+        [Test]
+        public void TestMostFrequentItemReturnZero()
+        {
+            int[] collection = { };
+            int output = MostFrequentItem.MostFrequentItemCount(collection);
+            Assert.AreEqual(0, output);
+        }
+    }
+}
