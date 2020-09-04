06a8738f1b5cb4be137755753fccf198720b9ec0
rodrigo espinoza <rodrigognr71@gmail.com>
2020-09-04 15:38:10 -0400

    Added Most Frequent Item Count excercise

---
+++ Katas/Rodrigo.Espinoza/FindCountOfMostFrequentItemInAnArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas.Rodrigo.Espinoza
+{
+    /// <summary>
+    /// Define Find Count of Most Frequent Item in an Array.
+    /// </summary>
+    public class FindCountOfMostFrequentItemInAnArray
+    {
+        /// <summary>
+        /// Return the most frequency item Qty.
+        /// </summary>
+        /// <param name="collection">Input array.</param>
+        /// <returns>Max Qty.</returns>
+        public static int MostFrequentItemCount(int[] collection)
+        {
+            if (collection.Length == 0 || collection == null) {
+                return 0;
+            }
+
+            var groups = collection.GroupBy(x => x);
+            var largest = groups.OrderByDescending(x => x.Count()).First();
+            return largest.Count();
+        }
+    }
+}
