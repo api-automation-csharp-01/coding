67ddea0a467ab9712d1fbeac8dd0e835cc6e369a
alex alcala <Alex.Alcala@jalasoft.com>
2020-09-04 02:26:23 -0400

    Most Frequent Item Solution

--- KatasTests/Alex.Alcala/MostFrequentItemTest.cs
+++ KatasTests/Alex.Alcala/MostFrequentItemTest.cs
@@ -10,7 +10,7 @@
     public class MostFrequentItemTest
     {
         /// <summary>
-        /// Tests MostFrequentItem with basic input.
+        /// Tests MostFrequentItem with an output of five.
         /// </summary>
         [Test]
         public void TestMostFrequentItem01()
@@ -18,6 +18,17 @@
             int[] input = new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 };
             int output = MostFrequentItem.MostFrequentItemCount(input);
             Assert.AreEqual(5, output);
+        }
+
+        /// <summary>
+        /// Tests MostFrequentItem with three inputs.
+        /// </summary>
+        [Test]
+        public void TestMostFrequentItem02()
+        {
+            int[] input = new int[] { 3, -1, -1 };
+            int output = MostFrequentItem.MostFrequentItemCount(input);
+            Assert.AreEqual(2, output);
         }
 
         /// <summary>
