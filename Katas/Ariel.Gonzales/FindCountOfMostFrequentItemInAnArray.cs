a4d5dbc04702636878a3c0a533a21ecb8c18f3b4
agonzales411 <agv417@gmail.com>
2020-09-03 20:07:53 -0400

    added first exercise MostFrequentItemCount

---
+++ Katas/Ariel.Gonzales/FindCountOfMostFrequentItemInAnArray.cs
@@ -0,0 +1,32 @@
+﻿namespace Katas.Ariel.Gonzales
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Define Find Count of Most Frequent Item in an Array.
+    /// </summary>
+    public class FindCountOfMostFrequentItemInAnArray
+    {
+        /// <summary>
+        /// Return the most frequency item Qty.
+        /// </summary>
+        /// <param name="collection">Input array.</param>
+        /// <returns>Max Qty.</returns>
+        public static int MostFrequentItemCount(int[] collection)
+        {
+            IDictionary<int, int> register = new Dictionary<int, int>();
+            if (collection.Length == 0)
+            {
+                return 0;
+            }
+
+            foreach (int num in collection)
+            {
+                register[num] = register.ContainsKey(num) ? ++register[num] : 1;
+            }
+
+            return register.Values.Max();
+        }
+    }
+}
