c701bce5f2cd5a9b660f3748cc1421f4880b2dc2
Edmundo Cossio <edmundo.cossio@jalasoft.com>
2020-09-04 15:59:17 -0400

    Most frequent item

---
+++ KatasTests/Edmundo.Cossio/FindFrequent.cs
@@ -0,0 +1,34 @@
+﻿namespace KatasTests.Edmundo.Cossio
+{
+    using Katas.Edmundo.Cossio;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines The find frequent
+    /// </summary>
+    [TestFixture]
+    public class FindFrequent
+    {
+        /// <summary>
+        /// Find Count of Most Frequent Item in an Array.
+        /// </summary>
+        [Test]
+        public void Test01()
+        {
+            int[] list = new int[] { 3, -1, -1 };
+            int result = MostFrequentItemCount.MostFrequentItemCount(list);
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// Find Count of Most Frequent Item in an Array.
+        /// </summary>
+        [Test]
+        public void Test02()
+        {
+            int[] list = new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 };
+            int result = MostFrequentItemCount.MostFrequentItemCount(list);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ KatasTests/Edmundo.Cossio/MostFrequentItemCount.cs
@@ -0,0 +1,23 @@
+﻿namespace Katas.Edmundo.Cossio
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines Most Frequent Item Count class.
+    /// </summary>
+    public class MostFrequentItemCount
+    {
+        /// <summary>
+        /// Gets the Count of Most Frequent Item in an Array
+        /// </summary>
+        /// <param name="elements">Array elements.</param>
+        /// <returns>result.</returns>
+        public static int MostFrequentItemCount(int[] elements)
+        {
+            var list = elements.GroupBy(val => val);
+            var result = list.OrderByDescending(val => val.Count()).First();
+            return result.Count();
+        }
+    }
+}
