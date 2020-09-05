4ae2d5f4b9e23875f7a4de077073648b9ba38d96
Jose Zambrana Rojas <emilio.12.jose@gmail.com>
2020-09-05 12:17:12 -0400

    Most Freq Item solution

---
+++ Katas/Emilio.Zambrana/MostFreqItems.cs
@@ -0,0 +1,57 @@
+﻿namespace Katas.Emilio.Zambrana
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Defines MOST class.
+    /// </summary>
+    public class MostFreqItems
+    {
+        /// <summary>
+        /// MostFreqItems.
+        /// </summary>
+        /// <param name="input">Input sequence.</param>
+        /// <returns>counter.</returns>
+        public static string GetFreqitem(int[] input)
+        {
+           if (input.Length == 0)
+            {
+                return 0;
+            }
+            else if (input.Length == 1)
+            {
+                return 1;
+            }
+
+           int mostreqitem = input[0];
+           int most = 0;
+           for (int i = 1; i < input.Length; i++)
+            {
+                if (GetMostFItemCounter(mostreqitem, input) < GetMostFItemCounter(input[i], input))
+                {
+                    mostreqitem = input[i];
+                    most = GetMostFItemCounter(input[i], input);
+                }
+            }
+
+            return most;
+        }
+         /// <summary>
+        /// Get Most Frequent Item Count.
+        /// </summary>
+        /// <param name="x">integer to compare.</param>
+        /// <param name="input">Input sequence.</param>
+        /// <returns>counter of the integer a.</returns>
+        public static int GetMostFItemCounter(int x, int[] input)
+        {
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (x == input[i])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
---
+++ KatasTests/Emilio.Zambrana/Mostfreqitemtest.cs
@@ -0,0 +1,24 @@
+﻿namespace KatasTests.Emilio.Zambrana
+{
+    using Katas.Emilio.Zambrana;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines MOST req item test.
+    /// </summary>
+    [TestFixture]
+    public class Mostfreqitemtest
+    {
+        /// <summary>
+        /// Tests freq items with basic input.
+        /// </summary>
+        [Test]
+        public void Test01()
+        {
+            int[] input = new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 };
+            int output = MostFreqItems.GetFreqitem(input);
+            Assert.AreEqual(5, output);
+        }
+
+    }
+}
