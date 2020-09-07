2e0bf78f536921175be63071adf2aaf7b0ce4a79
Edmundo Cossio <edmundo.cossio@jalasoft.com>
2020-09-07 10:37:14 -0400

    [develop] Edmundo Cossio - Most frequent item

---
+++ KatasTests/Edmundo.Cossio/FindFrequent.cs
@@ -0,0 +1,36 @@
+﻿namespace KatasTests.Edmundo.Cossio
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    /// <summary>
+    /// Defines The find frequent
+    /// </summary>
+    class FindFrequent
+    {
+        /// <summary>
+        /// Find Count of Most Frequent Item in an Array.
+        /// </summary>
+        [Test]
+        public void Test01()
+        {
+            int[] list = new int[] { 3, -1, -1 };
+            int result = MostFrequentItem.MostFrequentItemCount(list);
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// Find Count of Most Frequent Item in an Array.
+        /// </summary>
+        [Test]
+        public void Test02()
+        {
+            int[] list = new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 };
+            int result = MostFrequentItem.MostFrequentItemCount(list);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
