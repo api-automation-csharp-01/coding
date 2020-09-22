6c4396b6a189cfda77b14c86036a48bb1929b4d1
agonzales411 <agv417@gmail.com>
2020-09-22 09:23:10 -0400

    remove duplicate words kata added

---
+++ Katas/Ariel.Gonzales/RemoveDuplicateWordsExercise.cs
@@ -0,0 +1,21 @@
+﻿namespace Katas.Ariel.Gonzales
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Define Remove Duplicate Words in an string text.
+    /// </summary>
+    public static class RemoveDuplicateWordsExercise
+    {
+        /// <summary>
+        /// Return string with unique text.
+        /// </summary>
+        /// <param name="text">Input string.</param>
+        /// <returns>String with unique text.</returns>
+        public static string RemoveDuplicateWords(string text)
+        {
+            var singularTexts = text.Split(" ").Distinct().ToList();
+            return string.Join(" ", singularTexts);
+        }
+    }
+}
---
+++ KatasTests/Ariel.Gonzales/RemoveDuplicateWordsExerciseTest.cs
@@ -0,0 +1,36 @@
+﻿namespace KatasTests.Ariel.Gonzales
+{
+    using Katas.Ariel.Gonzales;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Define Remove Duplicate Words in an string text tests.
+    /// </summary>
+    [TestFixture]
+    public class RemoveDuplicateWordsExerciseTest
+    {
+        /// <summary>
+        /// Tests RemoveDuplicateWords with string text as input.
+        /// </summary>
+        [Test]
+        public void RemoveDuplicateWords_StringInput_ReturnString()
+        {
+            string input = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta";
+            string output = RemoveDuplicateWordsExercise.RemoveDuplicateWords(input);
+            string expectedOutput = "alpha beta gamma delta";
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        /// <summary>
+        /// Tests RemoveDuplicateWords with string text with no differences as input.
+        /// </summary>
+        [Test]
+        public void RemoveDuplicateWords_StringInput_ReturnSameString()
+        {
+            string input = "alpha beta gamma";
+            string output = RemoveDuplicateWordsExercise.RemoveDuplicateWords(input);
+            string expectedOutput = "alpha beta gamma";
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
