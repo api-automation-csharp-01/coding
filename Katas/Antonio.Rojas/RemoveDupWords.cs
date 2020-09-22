4a3ce8aa91a6338751b689971f25c3231ee2517c
Antonio Rojas <arojas@pros.com>
2020-09-22 17:00:31 -0400

    [develop] Antonio Rojas - Remove duplicate words

---
+++ Katas/Antonio.Rojas/RemoveDupWords.cs
@@ -0,0 +1,26 @@
+﻿namespace Katas.Antonio.Rojas
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Defines RemoveDupWords utility class.
+    /// </summary>
+    public class RemoveDupWords
+    {
+        /// <summary>
+        /// Gets string without duplicated words.
+        /// </summary>
+        /// <param name="input">Input string.</param>
+        /// <returns>String without duplicated words.</returns>
+        public static string RemoveDuplicatedWords(string input)
+        {
+            string output;
+            string[] arrayString = input.Split(' ');
+            var wordsuinque =
+            from str in arrayString
+            select str;
+            output = string.Join(" ", wordsuinque.Distinct().ToArray());
+            return output;
+        }
+    }
+}
---
+++ KatasTests/Antonio.Rojas/RemoveDupWordsTest.cs
@@ -0,0 +1,34 @@
+﻿namespace KatasTests.Antonio.Rojas
+{
+    using Katas.Antonio.Rojas;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines remove duplicated words test.
+    /// </summary>
+    [TestFixture]
+    public class RemoveDupWordsTest
+    {
+        /// <summary>
+        /// Tests remove duplicated words with basic input.
+        /// </summary>
+        [Test]
+        public void TestRemoveDupWordsCase01()
+        {
+            string input = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta";
+            string output = RemoveDupWords.RemoveDuplicatedWords(input);
+            Assert.AreEqual("alpha beta gamma delta", output);
+        }
+
+        /// <summary>
+        /// Tests remove duplicated words with basic input.
+        /// </summary>
+        [Test]
+        public void TestRemoveDupWordsCase02()
+        {
+            string input = "alpha beta gamma";
+            string output = RemoveDupWords.RemoveDuplicatedWords(input);
+            Assert.AreEqual("alpha beta gamma", output);
+        }
+    }
+}
