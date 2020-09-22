c63b7c128daab5d3f4821e31529328c988271a9f
alex alcala <Alex.Alcala@jalasoft.com>
2020-09-22 09:25:14 -0400

    Remove duplicate words solution

---
+++ KatasTests/Alex.Alcala/RemoveDuplicateWordsTest.cs
@@ -0,0 +1,24 @@
+﻿namespace KatasTests.Alex.Alcala
+{
+    using Katas.Alex.Alcala;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Defines RemoveDuplicateWords test.
+    /// </summary>
+    [TestFixture]
+    public class RemoveDuplicateWordsTest
+    {
+        /// <summary>
+        /// Tests RemoveDuplicateWords with an output of only single first words entries.
+        /// </summary>
+        [Test]
+        public void TestRemoveDuplicateWords01()
+        {
+            string words = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta";
+            string output = RemoveDuplicateWords.RemoveDuplicateWord(words);
+            string expected = "alpha beta gamma delta";
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
