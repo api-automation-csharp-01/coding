3eddc54075e34c310b4f188cec74b39b8d1f126d
dxni <33109901+dxni@users.noreply.github.com>
2020-09-22 20:46:12 -0400

    Task 04 - TDD Programming Exercise
    Daniela Ticona - Remove duplicate words

---
+++ Katas/Daniela.Ticona/RemoveDuplicate.cs
@@ -0,0 +1,20 @@
+﻿namespace Katas.Daniela.Ticona
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Defines RemoveDuplicate utility class.
+    /// </summary>
+    public class RemoveDuplicate
+    {
+        /// <summary>
+        /// Get Most Frequent Item.
+        /// </summary>
+        /// <param name="s"">Input string.</param>
+        /// <returns>String without duplicates words.</returns>
+        public static string RemoveDuplicateWords(string s)
+        {
+            return string.Join(" ", s.Split(' ').Distinct());
+        }
+    }
+}
---
+++ KatasTests/Daniela.Ticona/RemoveDuplicateTests.cs
@@ -0,0 +1,49 @@
+﻿namespace KatasTests.Daniela.Ticona
+{
+    using System;
+    using Katas.Daniela.Ticona;
+    using NUnit;
+    using NUnit.Framework;
+
+   /// <summary>
+    /// Test.
+    /// </summary>
+    public class RemoveDuplicateTests
+    {
+        /// <summary>
+        /// Tests remove dublicates on large string.
+        /// </summary>
+        [Test]
+        public void RemoveDublicatesOnLargeString()
+        {
+            string cadenaIn = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta";
+            string cadenaOut = RemoveDuplicate.RemoveDuplicateWords(cadenaIn);
+            Console.WriteLine(cadenaOut);
+            Assert.AreEqual("alpha beta gamma delta", cadenaOut);
+        }
+
+        /// <summary>
+        /// Tests remove dublicates on short string.
+        /// </summary>
+        [Test]
+        public void RemoveDublicatesOnShortString()
+        {
+            string cadenaIn = "Tres gatos miau";
+            string cadenaOut = RemoveDuplicate.RemoveDuplicateWords(cadenaIn);
+            Console.WriteLine(cadenaOut);
+            Assert.AreEqual("Tres gatos miau", cadenaOut);
+        }
+
+        /// <summary>
+        /// Test Remove duplicates in a string with single word.
+        /// </summary>
+        [Test]
+        public void RemoveDuplicatesInASingleword()
+        {
+            string cadenaIn = "gato";
+            string cadenaOut = RemoveDuplicate.RemoveDuplicateWords(cadenaIn);
+            Console.WriteLine(cadenaOut);
+            Assert.AreEqual("gato", cadenaOut);
+        }
+    }
+}
